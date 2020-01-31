6aed9f5515846c04479401c87974b87a3633b95b
Lng123 <louisng1@hotmail.com>
2020-01-30 21:37:55 -0800

    collapse independant

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react';
 import {Grid, Cell, Card, CardTitle, CardActions, Button, CardText, CardMenu, IconButton} from 'react-mdl';
-import {Collapse} from 'react-collapse';
 import { Carousel } from 'react-bootstrap';
 import './css/Projects.css';
 import argos1 from '../img/Argos1.png';
+import argos2 from '../img/Argos2.png';
+import argos3 from '../img/Argos3.png';
+import Collapse from './Collapse.js'
 
 class Projects extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {buttonStatus: false, isOpened: false, activeTab: 0};
+        this.state = {buttonStatus: false, isOpened: [false, false], activeTab: 0};
         this.handleClick = this.handleClick.bind(this);
         
     }
@@ -31,7 +33,7 @@ class Projects extends Component {
                     <div className="projects-grid">
                     <Card id="p1" shadow={5} style={{minWidth: '450px', height: 'auto'}}>
                     <CardText>
-                        <span>Argos</span>
+                        <span>Android Project: Argos</span>
                     </CardText>
                     
                     <Carousel interval={false}>
@@ -40,43 +42,69 @@ class Projects extends Component {
                     </Carousel.Item>
 
                     <Carousel.Item>
-                    <CardTitle style={{height: '300px'}}>Android Project 1
-                        </CardTitle>
+                        <img src={argos2}/>
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                        <img src={argos3}/>
+                    </Carousel.Item>
+
+                    </Carousel>
+                    <Collapse>
+                        <CardText>
+                        <h2>Argos</h2>
+                        An android app named Argos, aimed at improving bike security in Vancouver. Collaborated in a group of three students,
+                        Argos won first place out of 11 teams at the BCIT android development competition under the community category.
+                        Argos helps users find secure places to park their bike, using maps showing locations of bike racks and bike theft iin Vancouver.
+                        <br/><br/>
+                        Argos uses the google maps api to display nearby locations of bike racks, as markers and clusters. The user can then toggle a setting,
+                        showing all the bike thefts, overlayed as a heatmap.
+                        <br/><br/>
+                        The datasets for both the bike racks and bike thefts are stored in firebase and populates the map based off latitude and longitude.
+                        Coordinates were not available for the bike thefts dataset, and therefore a geocoding api was used to convert addresses to coordinates.
+                        <br></br>
+                        <h4>Technologies</h4>
+                        Java, Firebase, Android Studio
+                        </CardText>
+                        </Collapse>
+                    </Card>
+                    <Card id="p1" shadow={5} style={{minWidth: '450px', height: 'auto'}}>
+                    <CardText>
+                        <span>TAOEX Club</span>
+                    </CardText>
+                    
+                    <Carousel interval={false}>
+                    <Carousel.Item>
+                        <img src={argos1}/>
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                        <img src={argos2}/>
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                        <img src={argos3}/>
                     </Carousel.Item>
 
                     </Carousel>
 
-                    <Collapse isOpened ={this.state.isOpened}>
+                    <Collapse >
                         <CardText>
-                                Test Test<br/>
-                                Test Test<br/>
-                                Test Test<br/>
-                                Test Test<br/>
-                                Test Test<br/>
-                                Test Test<br/>
-                                Test Test<br/>
-                                Test Test<br/>
-                                Test Test<br/>
-                                Test Test<br/>
-                                Test Test<br/>
-                                Test Test<br/>
+                        <h2>TAOEX Club</h2>
+                        I had the oppurtunity to work on TAOEX Club for our client TAOEX along with five other students as part of the Industry Sponsored Student Projects program at BCIT
+                        TAOEX Club is a social webapp for the game taoex. 
+                        TAOEX Club is used to manage user profiles, matches, clubs, and leagues, and was built with Laravel, a PHP framework, while using MySQL as the database.
+
+                        <br/><br/>
+                        We were tasked with adding new features to the platform. The new features involved developing an 
+                        admistrator level features that has full control of the players such as banning, messaging, editing user profiles, and managing clubs, club leaders and members.
+                        We also added user level features such as owning and joining multiple clubs, club invitations, removing club members, club scores and rankings, and matches.
+                        <br/><br/>
+
+                        <h4>Technologies</h4>
+                        Laravel PHP Framework, MySql
                         </CardText>
                         </Collapse>
-                        <CardActions border>
-                            <Button colored onClick = {this.handleClick}>More Info</Button>
-                            <Button colored>GitHub</Button>
-                        </CardActions>
-                    </Card>
-                    <Card shadow={5} style={{minWidth: '450px'}}>
-                        <CardTitle style={{height: '300px'}}>Android Project 1
-                        </CardTitle>
-                        <CardText>
-                            Test Test
-                        </CardText>
-                        <CardActions border>
-                            <Button colored>More Info</Button>
-                            <Button colored>GitHub</Button>
-                        </CardActions>
                     </Card>
                 </div>
                     </Cell>
